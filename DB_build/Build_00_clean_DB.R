9f278f0cdb6802522c57118fa536ecba600cc26a
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-05-15 15:24:14 +0300

    Commit 2024-05-15 15:24

---
+++ DB_build/Build_00_clean_DB.R
@@ -0,0 +1,168 @@
+#!/usr/bin/env Rscript
+# /* Copyright (C) 2022 Athanasios Natsis <natsisphysicist@gmail.com> */
+#' ---
+#' title:         "Clean records"
+#' author:
+#'   - Natsis Athanasios^[natsisphysicist@gmail.com]
+#'
+#' documentclass:  article
+#' classoption:    a4paper,oneside
+#' fontsize:       10pt
+#' geometry:       "left=0.5in,right=0.5in,top=0.5in,bottom=0.5in"
+#' link-citations: yes
+#' colorlinks:     yes
+#'
+#' header-includes:
+#' - \usepackage{caption}
+#' - \usepackage{placeins}
+#' - \captionsetup{font=small}
+#'
+#' output:
+#'   bookdown::pdf_document2:
+#'     number_sections: no
+#'     fig_caption:     no
+#'     keep_tex:        yes
+#'     latex_engine:    xelatex
+#'     toc:             yes
+#'     toc_depth:       4
+#'     fig_width:       7
+#'     fig_height:      4.5
+#'   html_document:
+#'     toc:             true
+#'     keep_md:         yes
+#'     fig_width:       7
+#'     fig_height:      4.5
+#'
+#' date: "`r format(Sys.time(), '%F')`"
+#'
+#' ---
+
+#+ echo=F, include=T
+
+#### Golden Cheetah read activities summary directly from individual files
+
+## __ Document options  --------------------------------------------------------
+
+#+ echo=FALSE, include=TRUE
+knitr::opts_chunk$set(comment    = ""       )
+knitr::opts_chunk$set(dev        = c("pdf")) ## expected option
+knitr::opts_chunk$set(out.width  = "100%"   )
+knitr::opts_chunk$set(fig.align  = "center" )
+knitr::opts_chunk$set(cache      =  FALSE   )  ## !! breaks calculations
+knitr::opts_chunk$set(fig.pos    = '!h'     )
+
+
+
+#+ echo=FALSE, include=TRUE
+## __ Set environment  ---------------------------------------------------------
+Sys.setenv(TZ = "UTC")
+tic <- Sys.time()
+Script.Name <- "~/CODE/training_location_analysis/DB_build/Build_00_clean_DB.R"
+
+if (!interactive()) {
+  dir.create("../runtime/", showWarnings = F, recursive = T)
+  pdf( file = paste0("../runtime/", basename(sub("\\.R$",".pdf", Script.Name))))
+}
+
+#+ echo=F, include=T
+library(data.table, quietly = TRUE, warn.conflicts = FALSE)
+library(arrow,      quietly = TRUE, warn.conflicts = FALSE)
+library(dplyr,      quietly = TRUE, warn.conflicts = FALSE)
+library(filelock,   quietly = TRUE, warn.conflicts = FALSE)
+library(lubridate,  quietly = TRUE, warn.conflicts = FALSE)
+
+source("./DEFINITIONS.R")
+
+
+
+##  Open dataset  --------------------------------------------------------------
+if (!file.exists(DATASET)) {
+  stop("NO DB!\n")
+}
+
+DB <- open_dataset(DATASET,
+                   partitioning  = c("year", "month"),
+                   unify_schemas = T)
+
+##  Set some measurements
+db_rows  <- unlist(DB |> tally() |> collect())
+db_files <- unlist(DB |> select(file) |> distinct() |> count() |> collect())
+db_days  <- unlist(DB |> select(time) |> mutate(time= as.Date(time)) |> distinct() |> count() |> collect())
+db_vars  <- length(names(DB))
+
+##  Get file list
+wehave <- DB |> select(file, filemtime, year, month) |> unique() |> collect() |> data.table()
+
+##  Check files exist
+wehave[, exists := file.exists(file)]
+
+##  Check for edits
+wehave[, currenct := filemtime == floor_date(file.mtime(file), unit = "seconds")]
+
+##  List of offending files
+removefl <- wehave[exists == F | currenct == F]
+
+
+
+if (nrow(removefl) > 0){
+
+  # DB |> filter(file %in% removefl$file) |> count() |> collect()
+  # DB |> filter(!file %in% removefl$file & year %in% removefl$year & month %in% removefl$month) |> count() |> collect()
+
+  ##  Rewrite changed only data without removed files
+  write_dataset(DB |> filter(!file %in% removefl$file &
+                               year %in% removefl$year &
+                               month %in% removefl$month),
+                DATASET,
+                compression            = DBcodec,
+                compression_level      = DBlevel,
+                format                 = "parquet",
+                partitioning           = c("year", "month"),
+                existing_data_behavior = "delete_matching",
+                hive_style             = F)
+
+
+  DB <- open_dataset(DATASET,
+                     partitioning  = c("year", "month"),
+                     unify_schemas = T)
+
+  ##  Set some measurements
+  new_rows  <- unlist(DB |> tally() |> collect())
+  new_files <- unlist(DB |> select(file) |> distinct() |> count() |> collect())
+  new_days  <- unlist(DB |> select(time) |> mutate(time= as.Date(time)) |> distinct() |> count() |> collect())
+  new_vars  <- length(names(DB))
+
+  cat("\n")
+  cat("New rows:   ",  new_rows - db_rows , "\n")
+  cat("New files:  ", new_files - db_files, "\n")
+  cat("New days:   ",  new_days - db_days , "\n")
+  cat("New vars:   ",  new_vars - db_vars , "\n")
+  cat("\n")
+  cat("Total rows: ",  new_rows, "\n")
+  cat("Total files:", new_files, "\n")
+  cat("Total days: ",  new_days, "\n")
+  cat("Total vars: ",  new_vars, "\n")
+}
+
+
+
+
+stop()
+##  TODO remove changed files from DB
+
+##  TODO remove deleted files from DB
+
+
+
+
+
+
+#' **END**
+#+ include=T, echo=F
+tac <- Sys.time()
+cat(sprintf("%s %s@%s %s %f mins\n\n", Sys.time(), Sys.info()["login"],
+            Sys.info()["nodename"], basename(Script.Name), difftime(tac,tic,units = "mins")))
+# if (difftime(tac,tic,units = "sec") > 30) {
+#   system("mplayer /usr/share/sounds/freedesktop/stereo/dialog-warning.oga", ignore.stdout = T, ignore.stderr = T)
+#   system(paste("notify-send -u normal -t 30000 ", Script.Name, " 'R script ended'"))
+# }
