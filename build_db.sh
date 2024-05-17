f78546b60c1553535e242f884de0131b1e51477a
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-05-17 13:11:19 +0300

    Commit 2024-05-17 13:11

--- build_db.sh
+++ build_db.sh
@@ -4,8 +4,9 @@
 #### Run database build sequence
 
 "./DB_build/Build_00_clean_DB.R"
-"./DB_build/Build_01_GoldenCheetah_activities_records_json.R"
+"./DB_build/Build_01_json_GoldenCheetah_activities.R"
 "./DB_build/Build_02_gpx_records.R"
+"./DB_build/Build_03_fit_garmin.R"
 
 
 
