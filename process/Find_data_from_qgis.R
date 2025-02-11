5970438d694efb16e12e8c0a833b1d4a92209798
thanasisn <11601717+thanasisn@users.noreply.github.com>
2025-02-11 09:09:33 +0200

    Commit 2025-02-11 09:09

--- process/Find_data_from_qgis.R
+++ process/Find_data_from_qgis.R
@@ -26,6 +26,7 @@ suppressPackageStartupMessages({
   library(gdata,      quietly = TRUE, warn.conflicts = FALSE)
   library(duckdb,     quietly = TRUE, warn.conflicts = FALSE)
   library(sf,         quietly = TRUE, warn.conflicts = FALSE)
+  library(ggplot2,    quietly = TRUE, warn.conflicts = FALSE)
 })
 
 source("./DEFINITIONS.R")
@@ -42,12 +43,57 @@ tbl(con, "files")   |> glimpse()
 tbl(con, "records") |> glimpse()
 
 points <- tbl(con, "records") |>
-  select(fid, X, Y, X_LON, Y_LAT, time)
+  select(fid, X, Y, X_LON, Y_LAT, time, kph_2D, dist_2D)
+# points |> tally()
+
+files <- tbl(con, "files")
+
+bad <- cbind(
+  st_read("~/GISdata/badplacesl.gpkg") |> st_coordinates(),
+  st_read("~/GISdata/badplacesl.gpkg")
+) |> data.table()
 
 
-bad <- st_read("~/GISdata/badplacesl.gpkg") |> data.table()
+points |>
+  filter(kph_2D > 100) |>
+  ggplot() +
+  geom_histogram(aes(kph_2D))
+
+points |>
+  filter(dist_2D > 1000) |>
+  ggplot() +
+  geom_histogram(aes(dist_2D))
 
 
+
+for (al in 1:nrow(bad)) {
+  ll <- bad[al,]
+  ll$X + ll$Resolution/2
+  ll$X - ll$Resolution/2
+
+  ## find the source of bad points
+  badpoints <- points |>
+    filter(X < ll$X + ll$Resolution/2 & X > ll$X - ll$Resolution/2) |>
+    filter(Y < ll$Y + ll$Resolution/2 & Y > ll$Y - ll$Resolution/2) |>
+    collect()
+  badfiles <- files |> filter(fid %in% badpoints$fid) |> collect()
+
+
+  ## display info on bad points
+  cat(badfiles$file, "\n")
+  pander::pander(badpoints)
+
+  ## edit bad points
+  if (badfiles$filetype == "gpx") {
+    # system(paste("gvim ", badfiles$file))
+    # format(badpoints$time[1], "%FT%T")
+    system(paste0("gvim -c \"silent! /", format(badpoints$time[1], "%FT%T"), "\" ", badfiles$file),
+           wait = TRUE)
+  }
+
+  ## TODO add bad points to exclusion list
+
+}
 
 stop()
 
@@ -59,7 +105,3 @@ stop()
 tac <- Sys.time()
 cat(sprintf("%s %s@%s %s %f mins\n\n", Sys.time(), Sys.info()["login"],
             Sys.info()["nodename"], basename(Script.Name), difftime(tac,tic,units = "mins")))
-# if (difftime(tac,tic,units = "sec") > 30) {
-#   system("mplayer /usr/share/sounds/freedesktop/stereo/dialog-warning.oga", ignore.stdout = T, ignore.stderr = T)
-#   system(paste("notify-send -u normal -t 30000 ", Script.Name, " 'R script ended'"))
-# }
